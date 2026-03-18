84bbe0208d920f2740fe2f600b027c6e008203d5
Purnapragya12 <122694140+Purnapragya12@users.noreply.github.com>
2026-03-18 11:53:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -287,7 +287,7 @@ st.sidebar.title("🔍 Property Filters")
 
 city=st.sidebar.selectbox(
 "City",
-["Patna","Coimbatore","Ayodhya","Delhi NCR","Mumbai","Bangalore","Chennai","Pondicherry","Neemrana","Munger","Prayagraj"]
+["Patna","Munger","Chennai","Hyderabad","Vishakhapatnam",,"Prayagraj","Coimbatore","Ayodhya","Delhi NCR","Mumbai","Bangalore","Pondicherry","Neemrana"]
 )
 
 budget=st.sidebar.slider(
